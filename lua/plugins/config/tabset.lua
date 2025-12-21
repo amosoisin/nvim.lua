8c1ce013310fba9552cde2eb82cd3f28440d5d2f
amosoisin <23148082+amosoisin@users.noreply.github.com>
2025-12-21 21:42:31 +0900

    Fix - Add tabset config

---
+++ lua/plugins/config/tabset.lua
@@ -0,0 +1,38 @@
+require("tabset").setup({
+  defaults = {
+    tabwidth = 4,
+    tabstop = 4,
+    expandtab = true
+  },
+  languages = {
+    go = {
+      tabwidth = 4,
+      expandtab = false
+    },
+    lua = {
+      tabwidth = 2,
+      tabstop = 2,
+    },
+    c = {
+      tabwidth = 8,
+      tabstop = 8,
+      expandtab = false,
+    },
+    html = {
+      tabwidth = 2,
+      tabstop = 2,
+    },
+    python = {
+      tabwidth = 4,
+      tabstop = 4,
+      expandtab = true,
+    },
+    {
+      filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact", "json", "yaml" },
+      config = {
+          tabwidth = 2,
+          tabstop = 2,
+      }
+    }
+  }
+})
