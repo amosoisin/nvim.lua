f85d3d49d080c4f7b3768b45352cc3b0e3619822
amosoisin <23148082+amosoisin@users.noreply.github.com>
2025-12-31 07:29:34 +0900

    rainbow-deliminatersプラグインを追加

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -83,5 +83,34 @@ return {
     config = function()
       require("plugins.config.eyeliner")
     end
+  },
+  {
+    -- 区切り文字をカラフルにする
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      vim.g.rainbow_delimiters = {
+          strategy = {
+              [''] = 'rainbow-delimiters.strategy.global',
+              vim = 'rainbow-delimiters.strategy.local',
+          },
+          query = {
+              [''] = 'rainbow-delimiters',
+              lua = 'rainbow-blocks',
+          },
+          priority = {
+              [''] = 110,
+              lua = 210,
+          },
+          highlight = {
+              'RainbowDelimiterRed',
+              'RainbowDelimiterYellow',
+              'RainbowDelimiterBlue',
+              'RainbowDelimiterOrange',
+              'RainbowDelimiterGreen',
+              'RainbowDelimiterViolet',
+              'RainbowDelimiterCyan',
+          },
+      }
+    end
   }
 }
