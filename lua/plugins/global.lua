1792cf4fc1c9531cc2d2c5e5d8fda2a2e4a57bc6
amosoisin <23148082+amosoisin@users.noreply.github.com>
2025-12-31 05:42:44 +0900

    accelerated-jkを追加
    jとkを押した時に加速するプラグインを追加

--- lua/plugins/global.lua
+++ lua/plugins/global.lua
@@ -121,4 +121,19 @@ return {
     cmd = "Atone",
     opts = {}, -- your configuration here
   },
+  {
+    -- jとkを押し続けた時に加速
+    'rainbowhxch/accelerated-jk.nvim',
+    config = function ()
+      require('accelerated-jk').setup({
+          mode = 'time_driven',
+          enable_deceleration = false,
+          acceleration_motions = {},
+          acceleration_limit = 150,
+          acceleration_table = { 7,12,17,21,24,26,28,30 },
+          -- when 'enable_deceleration = true', 'deceleration_table = { {200, 3}, {300, 7}, {450, 11}, {600, 15}, {750, 21}, {900, 9999} }'
+          deceleration_table = { {150, 9999} }
+      })
+    end
+  },
 }
